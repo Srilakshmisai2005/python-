87662df98bbded15464afed3bd32ca54f6b39739
Srilakshmisai2005 <121482153+Srilakshmisai2005@users.noreply.github.com>
2022-12-28 01:20:04 -0800

    Add files via upload

---
+++ sum and avg.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number:"))
+sum=0
+for a in range(1,n+1,1):
+ sum=sum+a
+ print("the sum of first",n,"number is",sum)
+ average=sum/a
+ print("average of",n,"number is",average)
