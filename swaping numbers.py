0b589e8966c5e7049786f54366696474c55b1df4
Srilakshmisai2005 <121482153+Srilakshmisai2005@users.noreply.github.com>
2022-12-28 01:20:59 -0800

    Add files via upload

---
+++ swaping numbers.py
@@ -0,0 +1,12 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+
+
+temp_1=a
+a=b
+b=temp_1
+
+
+print("value of a after swap is:",a)
+print("value of b after awap is:",b)
+      
