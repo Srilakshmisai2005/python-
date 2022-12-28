a3f479b751c35d7925519669f1c0e585a67f2f4f
Srilakshmisai2005 <121482153+Srilakshmisai2005@users.noreply.github.com>
2022-12-28 01:14:34 -0800

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,9 @@
+a=int(input("enter tha value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+
+s=(a+b+c)/2
+
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print("the area of triangle is:",area)
+
